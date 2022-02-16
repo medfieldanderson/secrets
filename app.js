77b43e5a8581ccd8fd1de2fdcf65d95e02e20867
Jeff <medfield.anderson@gmail.com>
2022-02-16 10:39:01 -0500

    Using bcrypt with salting

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -38,35 +38,41 @@ app.get("/register", (req, res)=> {
 });
 
 app.post("/register", async (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  try{
-    const savedUser = await newUser.save();
-    if(savedUser) {
-      console.log(`Successfully saved ${savedUser}`);
+
+  bcrypt.hash(req.body.password, saltRounds, async (err, hash) =>{
+
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    try{
+      const savedUser = await newUser.save();
+      if(savedUser) {
+        console.log(`Successfully saved ${savedUser}`);
+      }
+      res.render("secrets");
+    } catch (error) {
+      console.log(error);
     }
-    res.render("secrets");
-  } catch (error) {
-    console.log(error);
-  }
+  });
 });
 
 app.post("/login", async (req, res) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   try {
     const foundUser = await User.findOne({email: username});
     if(foundUser) {
       console.log(`Successfully found user ${foundUser} ...`);
-      if(foundUser.password === password) {
-        console.log(`... and successfully Logged In`);
-        res.render("secrets");
-      } else {
-        console.log(`WRONG PASSWORD!`);
-      }
+      bcrypt.compare(password, foundUser.password, async (err, result) => {
+        if(result === true) {
+          console.log(`... and successfully Logged In`);
+          res.render("secrets");
+        } else {
+          console.log(`WRONG PASSWORD!`);
+        }
+      });
     } else {
       console.log(`${username} NOT FOUND`);
     }
